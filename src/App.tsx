6f5f29f8896bcca3cc33d7c250e2b4e115bda75f
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-07-30 00:37:45 +0530

    Install chakra UI and its components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Box, Text } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <>
+    <Box>
+      <Text>bb</Text>
+    </Box>
     </>
   )
 }
